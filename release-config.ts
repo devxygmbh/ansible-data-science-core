283905c07fdf0e98e7856115a7783433bf1cc1c5
pat-s <patrick.schratz@gmail.com>
2024-10-04 01:34:03 +0200

    chore: update release config

--- release-config.ts
+++ release-config.ts
@@ -2,4 +2,6 @@ export default {
   commentOnReleasedPullRequests: false,
   // to process renovate's automerge: branch updates
   skipCommitsWithoutPullRequest: false,
+  releasePrefix: "WIP: Changelog for",
+  includeContributors: false
 };
